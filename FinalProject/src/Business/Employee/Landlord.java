fdcd7e8e7dda4373cced1a34a831a0be162a4190
YiningChen001586646 <chen.yinin@northeastern.edu>
2020-11-22 11:37:37 -0500

    create landlord

--- FinalProject/src/Business/Employee/Landlord.java
+++ FinalProject/src/Business/Employee/Landlord.java
@@ -12,7 +12,7 @@ import Business.Employee.*;
  */
 public class Landlord {
     
-    private String name;
+
     private int id;
     private static int count = 1;
     private String firstname;
@@ -62,18 +62,11 @@ public class Landlord {
         return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    
-    public String getName() {
-        return name;
-    }
 
     @Override
     public String toString() {
-        return name;
+        return lastname;
     }
     
     
