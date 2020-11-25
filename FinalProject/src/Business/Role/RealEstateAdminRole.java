21961789a3352b1b35e7309b212a8022cdb249c1
Henry Huang <Mchighlight@gmail.com>
2020-11-25 01:50:44 -0500

    add property admin work area panel

--- FinalProject/src/Business/Role/RealEstateAdminRole.java
+++ FinalProject/src/Business/Role/RealEstateAdminRole.java
@@ -4,15 +4,15 @@ import Business.EcoSystem;
 import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
-import ui.admin.RealEstateAdminWorkAreaJPanel;
+import ui.PropertyCompanyAdminRole.RealEstateAdminWorkAreaJPanel;
 import javax.swing.JPanel;
 
 
 public class RealEstateAdminRole extends Role{
 
     @Override
-    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem system) {
-        return new RealEstateAdminWorkAreaJPanel(userProcessContainer, system);
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        return new RealEstateAdminWorkAreaJPanel(userProcessContainer,business, enterprise);
     }
     
 }
